2acde1aa846b9245af3d7c06531564ec1a8dde5b
Ibrahim <edinstvenniy123@gmail.com>
2023-12-04 00:24:21 +0300

    implement url saving

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -5,6 +5,10 @@ import (
 	"os"
 
 	"github.com/mirhijinam/url-shortener/internal/config"
+	"github.com/mirhijinam/url-shortener/internal/lib/logger/sl"
+	"github.com/mirhijinam/url-shortener/internal/storage/sqlite"
+	_ "github.com/mirhijinam/url-shortener/internal/storage/sqlite"
+
 	"golang.org/x/exp/slog"
 )
 
@@ -16,8 +20,31 @@ const (
 
 func main() {
 	cfg := config.MustLoad()
-
 	fmt.Println("Config: ", cfg)
+
+	log := SetupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		slog.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	id, err := storage.SaveURL("https://google.com", "google")
+	if err != nil {
+		log.Error("failed to save url", sl.Err(err))
+	}
+	log.Info("saved url", slog.Int64("id", id))
+
+	id, err = storage.SaveURL("https://google.com", "google")
+	if err != nil {
+		log.Error("failed to save url", sl.Err(err))
+	}
+
+	_ = storage
 }
 
 func SetupLogger(env string) *slog.Logger {
